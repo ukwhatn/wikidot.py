07924f8f134051c9775bbe919a8cb2254148ebb7
ukwhatn <ukwhatn@gmail.com>
2021-02-14 21:38:22 +0900

    [tagreplace] bugfix again again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='wikidot',
-    version='0.1.55',
+    version='0.1.56',
     description='Wikidot AMC utility',
     author='ukwhatn',
     author_email='ukwhatn@gmail.com',
