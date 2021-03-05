4d6e62af5c8b4e5cfdf6d7673ef7048e0956b572
ukwhatn <ukwhatn@gmail.com>
2021-03-05 21:25:45 +0900

    setup.pyにlxmlを追記

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='wikidot',
-    version='0.1.6',
+    version='0.1.61',
     description='Wikidot AMC utility',
     author='ukwhatn',
     author_email='ukwhatn@gmail.com',
@@ -12,6 +12,7 @@ setuptools.setup(
     install_requires=[
         "bs4==0.0.1",
         "feedparser==6.0.2",
-        "httpx==0.16.1"
+        "httpx==0.16.1",
+        "lxml==4.6.2"
     ]
 )
