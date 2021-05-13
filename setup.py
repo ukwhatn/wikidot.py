9fd1a7b013362bae894acd4f8077c177750ecc60
ukwhatn <ukwhatn@gmail.com>
2021-05-13 19:23:21 +0900

    up: 0.1.664

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='wikidot',
-    version='0.1.663',
+    version='0.1.664',
     description='Wikidot AMC utility',
     author='ukwhatn',
     author_email='ukwhatn@gmail.com',
