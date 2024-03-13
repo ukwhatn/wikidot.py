aee3dcdaac0af9a7b712892ed8c44356b047d21c
Yuki Watanabe <ukwhatn@gmail.com>
2024-03-14 00:40:16 +0900

    fix: login_requiredデコレータ改修

--- wikidot/common/decorators.py
+++ wikidot/common/decorators.py
@@ -1,17 +1,38 @@
 from functools import wraps
+
+from wikidot.module.client import Client
 from .exceptions import LoginRequiredException
 
 
 def login_required(func):
     """ログインが必要な関数につけるデコレータ
 
-    self.client.is_logged_in()がFalseの場合、LoginExceptionをraiseする
+    関数のパラメータに存在するclientを取得し、ログインしていない場合は例外を送出する
+    clientはパラメータ、もしくはself.clientで取得できる
     """
 
     @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        if not self.client.is_logged_in():
-            raise LoginRequiredException("You must be logged in to use this function.")
-        return func(self, *args, **kwargs)
+    def wrapper(*args, **kwargs):
+        client = None
+        if 'client' in kwargs:
+            client = kwargs['client']
+        else:
+            for arg in args:
+                if isinstance(arg, Client):
+                    client = arg
+                    break
+
+            # selfに存在するか？
+            if client is None:
+                if hasattr(args[0], 'client'):
+                    client = args[0].client
+
+        if client is None:
+            raise ValueError('Client is not found')
+
+        if not client.is_logged_in:
+            raise LoginRequiredException('Login is required to execute this function')
+
+        return func(*args, **kwargs)
 
     return wrapper
